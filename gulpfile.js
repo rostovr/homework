75d2f968f6df6605899b8b98361d738aea1c72ae
rostovr <33627819+rostovr@users.noreply.github.com>
2017-12-11 16:23:28 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 //подключение пакетов
 
 var gulp=require('gulp');
+var gutil = require('gulp-util'); //нужен для обработки ошибок
 var minCSS=require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
+var smartgrid = require('smart-grid');
 
 
 gulp.task('less1', function(){
 	gulp.src('less/*.less')
-		.pipe(less())
+		.pipe(less().on('error', gutil.log))
 		.pipe(gulp.dest('assets/css'));
 });
 
@@ -27,6 +29,8 @@ gulp.task('min', function(){
 });
 
 // Static Server + watching scss/html files
+// Запуск задачи мониторинга и компилляции gulp serve
+// Если надо только мониторинг с компилляцией без синка в браузере(например на c9)
 gulp.task('serve', ['less'], function() {
 
     browserSync.init({
@@ -38,12 +42,68 @@ gulp.task('serve', ['less'], function() {
     gulp.watch("app/assets/css/*.css").on('change', browserSync.reload); //мониторим css
 });
 
+
 // Compile sass into CSS & auto-inject into browsers
+// ---Только компилляция gulp less
+// ---Если нужна только разовая компилляция, без всяких автоматов
+
 gulp.task('less', function() {
     return gulp.src("app/less/*.less")
-        .pipe(less())
+        .pipe(less().on('error', gutil.log))
         .pipe(gulp.dest("app/assets/css"))
         .pipe(browserSync.stream());
 });
 
+// ---Запуск задачи мониторинга, компилляции и browser-sync.
+// ---Запускать преимущественно только эту задачу.
+
 gulp.task('auto', ['serve']);
+
+////////////////////////////////////---SMARTGRID---//////////////////////////
+///////
+/* It's principal settings in smart grid project. Базовые настройки для генерации библиотеки */
+
+var settings = {
+    outputStyle: 'less', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '30px', /* gutter width px || % */
+    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+        },
+        md: {
+            width: '960px'
+        },
+        sm: {
+            width: '780px',
+            fields: '15px' /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '560px'
+        }
+        /* 
+        We can create any quantity of break points.
+
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+
+// --- Генерация библиотеки с настройками settings в указанную папку 
+// gulp grid
+
+gulp.task('grid', function(){
+	smartgrid('app/scr/', settings);
+});
+
+
+
